3976db98c2dda846b121f39dee11aa52bab8a4c2
inneezzz <innahenitacahyanurani.com>
2024-09-11 15:22:57 +0700

    Praktikum 5B sampai 5E Penggunaan Require dan Include

--- empat.php
+++ empat.php
@@ -1,56 +1,8 @@
 <?php
 
-class Author {
-    public $name;
-    public $description;
-
-}
-
-class Publisher {
-    public $name;
-    public $address;
-    public$phone;
-
-    public function setPhone($phone) {
-        $this->phone = $phone;
-    }
-
-    public function getPhone() {
-        return $this->phone;
-    }
-
-}
-
-class Book {
-    public $ISBN;
-    public $title;
-    public $description;
-    public $category;
-    public $language;
-    public $numberOfPage;
-    public $author;
-    public $publisher;
-
-    public function showAll() {
-        return [
-            'ISBN' => $this->ISBN,
-            'Title' => $this->title,
-            'Description' => $this->description,
-            'Language' => $this->language,
-            'Number of Page' => $this->numberOfPage,
-            'Author' => $this->author,
-            'Publisher' => $this->publisher,
-        ];
-        
-    }
-
-    public function detail($ISBN) {
-        if ($this->ISBN == $ISBN) {
-            return $this->showAll();
-        }
-        return null;
-    }
-}
+require_once 'Author.php';
+require_once 'Publisher.php';
+require_once 'Book.php'; 
 
 $author = new Author();
 $author->name = "Tere Liye";
--- keenam.php
+++ keenam.php
@@ -1,3 +1,5 @@
 <?php
-include 'kelima.php';
+require_once 'kelima.php';
+require_once 'kelima.php';
+require_once 'ketujuh.php';
 echo "Ini file keenam.php\n";
